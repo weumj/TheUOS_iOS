a87bcfddb095bd30f95b1a58cfe39968c5056a94
Myungjin Kim <weumj1@gmail.com>
2017-03-26 02:49:01 +0900

    Initial Commit

---
+++ TheUOSTests/TheUOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  TheUOSTests.swift
+//  TheUOSTests
+//
+//  Created by MJ on 2017. 3. 26..
+//  Copyright © 2017년 uoscs09. All rights reserved.
+//
+
+import XCTest
+@testable import TheUOS
+
+class TheUOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
